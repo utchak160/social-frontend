b6fdabff5e75bb8f6a90b0565d2ee475a10c75e5
utchak160 <chakravartyutkarsh@gmail.com>
2020-07-20 21:03:12 +0530

    form hook done

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from 'react-router-dom';
 
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(formData);
+    }
     return (
         <section className="container">
-            <div className="alert alert-danger">
-                Invalid credentials
-            </div>
+            {/*<div className="alert alert-danger">*/}
+            {/*    Invalid credentials*/}
+            {/*</div>*/}
             <h1 className="large text-primary">Sign In</h1>
             <p className="lead"><i className="fas fa-user"/> Sign into Your Account</p>
-            <form className="form" action="dashboard.html">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                     <input
                         type="email"
                         placeholder="Email Address"
                         name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
                         required
                     />
                 </div>
@@ -24,6 +41,8 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
                     />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Login"/>
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from 'react-router-dom';
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const {name, email, password, password2} = formData;
+
+    const onChange = e => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if (password !== password2) {
+            console.log('Password do not match');
+            return '';
+        }
+        console.log(formData);
+    };
+
     return (
         <section className="container">
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead"><i className="fas fa-user"/> Create Your Account</p>
-            <form className="form" action="create-profile.html">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" required/>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={name}
+                        onChange={e => onChange(e)}
+                        required/>
                 </div>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email"/>
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        name="email"
+                        required
+                    />
                     <small className="form-text"
                     >This site uses Gravatar so if you want a profile image, use a
                         Gravatar email</small
@@ -23,6 +58,8 @@ const Register = () => {
                         placeholder="Password"
                         name="password"
                         minLength="6"
+                        value={password}
+                        onChange={e => onChange(e)}
                     />
                 </div>
                 <div className="form-group">
@@ -31,6 +68,8 @@ const Register = () => {
                         placeholder="Confirm Password"
                         name="password2"
                         minLength="6"
+                        value={password2}
+                        onChange={e => onChange(e)}
                     />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Register"/>
